181065cca0b98dd4e35042e4c5b763516c4d4d18
gayashan4lk <gayashan4lk@gmail.com>
2024-05-05 11:20:05 +0530

    reformat file structure

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,6 +1,6 @@
 import {cookies} from 'next/headers'
 
-import {CreatePost} from '~/app/_components/create-post'
+import {CreatePost} from '~/components/create-post'
 import {api} from '~/trpc/server'
 import {createClient} from '~/utils/supabase/server'
 
