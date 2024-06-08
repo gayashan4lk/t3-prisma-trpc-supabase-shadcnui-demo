7d5fc3556fc5ded146a8f2dc40a29f8e6e8b5726
gayashan4lk <gayashan4lk@gmail.com>
2024-06-08 16:27:11 +0530

    minor update

--- src/app/(auth)/auth/callback/route.ts
+++ src/app/(auth)/auth/callback/route.ts
@@ -5,6 +5,7 @@ import {createClient} from '~/utils/supabase/server'
 export async function GET(req: Request) {
     const reqUrl = new URL(req.url)
     const code = reqUrl.searchParams.get('code')
+    console.log('reqUrl', reqUrl)
 
     if (code) {
         const supabase = createClient()
